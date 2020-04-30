e30fbb7ccf3c6706020f77d62dc4818ce0b42064
miguel bravo <miguelbravo153@gmail.com>
2020-04-29 23:50:13 -0500

    sequelize!

---
+++ server/config/database.js
@@ -0,0 +1,9 @@
+const { Sequelize } = require('sequelize')
+
+const sequelize = new Sequelize('sequelize', 'mabravo153', 'Barranquilla1.', {
+    host: 'pruebasproyecto.cnvwfa5zqlmr.us-east-2.rds.amazonaws.com',
+    dialect: 'mariadb'
+})
+
+
+module.exports = sequelize 
---
+++ server/config/index.js
@@ -0,0 +1,8 @@
+module.exports = {
+    development: {
+        siteName: 'Desarrollo'
+    },
+    production: {
+        siteName: 'Viajes'
+    }
+}
